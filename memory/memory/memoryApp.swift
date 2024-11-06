85166d0dfcfc0bfd75ba3f5b06226de80f53c481
Yzr <Yzrrrr1212@gmail.com>
2024-11-06 19:44:08 +0800

    first commit

---
+++ memory/memory/memoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  memoryApp.swift
+//  memory
+//
+//  Created by 易泽仁 on 2024/10/15.
+//
+
+import SwiftUI
+
+@main
+struct memoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ memory/memoryTests/memoryTests.swift
@@ -0,0 +1,17 @@
+//
+//  memoryTests.swift
+//  memoryTests
+//
+//  Created by 易泽仁 on 2024/10/15.
+//
+
+import Testing
+@testable import memory
+
+struct memoryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
